94754ed85f00028e7c66741815782a9511ef0d99
ramadhanialfarizi <ramadhani.alfarizi@gmail.com>
2024-09-17 10:24:53 +0700

    add tv repository implementation

--- lib/data/repositories/tv_repository_impl.dart
+++ lib/data/repositories/tv_repository_impl.dart
@@ -1,3 +1,12 @@
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/common/failure.dart';
+import 'package:ditonton/data/models/tv_response/popular/tv_popular_list_response.dart';
 import 'package:ditonton/domain/repositories/tv_repository.dart';
 
-class TvRepositoryImpl implements TvRepository {}
+class TvRepositoryImpl implements TvRepository {
+  @override
+  Future<Either<Failure, TvPopularListResponse>> getPopularTvShow() {
+    // TODO: implement getPopularTvShow
+    throw UnimplementedError();
+  }
+}
--- lib/domain/repositories/tv_repository.dart
+++ lib/domain/repositories/tv_repository.dart
@@ -1,1 +1,7 @@
-abstract class TvRepository {}
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/common/failure.dart';
+import 'package:ditonton/data/models/tv_response/popular/tv_popular_list_response.dart';
+
+abstract class TvRepository {
+  Future<Either<Failure, TvPopularListResponse>> getPopularTvShow();
+}
