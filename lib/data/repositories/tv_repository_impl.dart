8b12a6fa6ee60498c5f7bd76d09ebfdf3047637e
ramadhanialfarizi <ramadhani.alfarizi@gmail.com>
2024-09-20 14:13:03 +0700

    update tv repository

--- lib/data/repositories/tv_repository_impl.dart
+++ lib/data/repositories/tv_repository_impl.dart
@@ -1,12 +1,41 @@
 import 'package:dartz/dartz.dart';
 import 'package:ditonton/common/failure.dart';
+import 'package:ditonton/data/models/tv_response/detail/tv_detail_response.dart';
+import 'package:ditonton/data/models/tv_response/on_the_air/tv_on_air_list_response.dart';
 import 'package:ditonton/data/models/tv_response/popular/tv_popular_list_response.dart';
+import 'package:ditonton/data/models/tv_response/recomendation/tv_recomendation_list_response.dart';
+import 'package:ditonton/data/models/tv_response/top_rated/tv_top_rated_list_response.dart';
 import 'package:ditonton/domain/repositories/tv_repository.dart';
 
 class TvRepositoryImpl implements TvRepository {
   @override
   Future<Either<Failure, TvPopularListResponse>> getPopularTvShow() {
     // TODO: implement getPopularTvShow
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, TvRecomendationListResponse>>
+      getRecomendationTvShow() {
+    // TODO: implement getRecomendationTvShow
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, TvTopRatedListResponse>> getTopRatedTvShow() {
+    // TODO: implement getTopRatedTvShow
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, TvOnAirListResponse>> getOnAirTvShow() {
+    // TODO: implement getOnAirTvShow
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, TvDetailResponse>> getDetailTvShow(int id) {
+    // TODO: implement getDetailTvShow
     throw UnimplementedError();
   }
 }
--- lib/domain/repositories/tv_repository.dart
+++ lib/domain/repositories/tv_repository.dart
@@ -1,7 +1,15 @@
 import 'package:dartz/dartz.dart';
 import 'package:ditonton/common/failure.dart';
+import 'package:ditonton/data/models/tv_response/detail/tv_detail_response.dart';
+import 'package:ditonton/data/models/tv_response/on_the_air/tv_on_air_list_response.dart';
 import 'package:ditonton/data/models/tv_response/popular/tv_popular_list_response.dart';
+import 'package:ditonton/data/models/tv_response/recomendation/tv_recomendation_list_response.dart';
+import 'package:ditonton/data/models/tv_response/top_rated/tv_top_rated_list_response.dart';
 
 abstract class TvRepository {
   Future<Either<Failure, TvPopularListResponse>> getPopularTvShow();
+  Future<Either<Failure, TvRecomendationListResponse>> getRecomendationTvShow();
+  Future<Either<Failure, TvTopRatedListResponse>> getTopRatedTvShow();
+  Future<Either<Failure, TvOnAirListResponse>> getOnAirTvShow();
+  Future<Either<Failure, TvDetailResponse>> getDetailTvShow(int id);
 }
