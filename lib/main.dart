0dd653364de51d4c8a60a611324aa134ea4aa4c7
ramadhanialfarizi <ramadhani.alfarizi@gmail.com>
2025-01-28 16:30:19 +0700

    fixing ios

--- lib/main.dart
+++ lib/main.dart
@@ -28,9 +28,7 @@ import 'package:ditonton/presentation/provider/tv_show/tv_top_rated_controller.d
 import 'package:ditonton/presentation/provider/tv_show/watchlist_tv_controller.dart';
 import 'package:ditonton/presentation/provider/watchlist_movie_notifier.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:ditonton/injection.dart' as di;
