5d3e7f783b3e23e9e84d86af40e8d1ffdb8630c3
ramadhanialfarizi <ramadhani.alfarizi@gmail.com>
2025-02-07 23:12:26 +0700

    change state management from provider to bloc in search movie

--- lib/injection.dart
+++ lib/injection.dart
@@ -28,6 +28,7 @@ import 'package:ditonton/domain/usecases/tv_show/get_watchlist_tv_status.dart';
 import 'package:ditonton/domain/usecases/tv_show/remove_watchlist_tv.dart';
 import 'package:ditonton/domain/usecases/tv_show/save_watchlist_tv.dart';
 import 'package:ditonton/domain/usecases/tv_show/search_tv_show.dart';
+import 'package:ditonton/presentation/bloc/movie/search/search_bloc.dart';
 import 'package:ditonton/presentation/provider/movie_detail_notifier.dart';
 import 'package:ditonton/presentation/provider/movie_list_notifier.dart';
 import 'package:ditonton/presentation/provider/movie_search_notifier.dart';
@@ -86,6 +87,14 @@ void init() {
     ),
   );
 
+  // Bloc
+  locator.registerFactory(
+    () => SearchBloc(
+      locator(),
+    ),
+  );
+
+  // Provider
   // <======== Tv show Provider =====================>
   locator.registerFactory(
     () => TvShowController(
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:ditonton/common/constants.dart';
 import 'package:ditonton/common/utils.dart';
 import 'package:ditonton/firebase_options.dart';
+import 'package:ditonton/presentation/bloc/movie/search/search_bloc.dart';
 import 'package:ditonton/presentation/pages/about_page.dart';
 import 'package:ditonton/presentation/pages/movie_detail_page.dart';
 import 'package:ditonton/presentation/pages/home_movie_page.dart';
@@ -30,6 +31,7 @@ import 'package:ditonton/presentation/provider/watchlist_movie_notifier.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
 import 'package:ditonton/injection.dart' as di;
 
@@ -86,7 +88,12 @@ class MyApp extends StatelessWidget {
         ),
         ChangeNotifierProvider(
           create: (_) => di.locator<WatchlistTvController>(),
-        )
+        ),
+
+        // BLOC State management
+        BlocProvider(
+          create: (_) => di.locator<SearchBloc>(),
+        ),
       ],
       child: MaterialApp(
         title: 'Flutter Demo',
