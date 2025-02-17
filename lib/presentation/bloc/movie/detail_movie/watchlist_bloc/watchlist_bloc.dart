ab19eaf6c735981b62bf1b40615b5b581b37aa75
ramadhanialfarizi <ramadhani.alfarizi@gmail.com>
2025-02-18 00:02:43 +0700

    updateeee

--- lib/presentation/bloc/movie/detail_movie/watchlist_bloc/watchlist_bloc.dart
+++ lib/presentation/bloc/movie/detail_movie/watchlist_bloc/watchlist_bloc.dart
@@ -23,7 +23,7 @@ class WatchlistBloc extends Bloc<WatchlistEvent, WatchlistState> {
       (event, emit) async {
         final isInWatchlist = await getWatchListStatus.execute(event.id);
 
-        emit(WatchlistUpdated(isInWatchlist, ""));
+        emit(WatchlistUpdated(isInWatchlist, "xxxx"));
       },
     );
 
