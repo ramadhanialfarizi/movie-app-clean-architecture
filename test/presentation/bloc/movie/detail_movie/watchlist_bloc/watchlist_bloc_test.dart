5dce5a7f716e0fb552bca85695f14d5424a4c515
ramadhanialfarizi <ramadhani.alfarizi@gmail.com>
2025-02-21 23:07:49 +0700

    cleaning code

--- test/presentation/bloc/movie/detail_movie/watchlist_bloc/watchlist_bloc_test.dart
+++ test/presentation/bloc/movie/detail_movie/watchlist_bloc/watchlist_bloc_test.dart
@@ -1,6 +1,5 @@
 import 'package:bloc_test/bloc_test.dart';
 import 'package:dartz/dartz.dart';
-import 'package:ditonton/common/exception.dart';
 import 'package:ditonton/common/failure.dart';
 import 'package:ditonton/domain/usecases/get_watchlist_status.dart';
 import 'package:ditonton/domain/usecases/remove_watchlist.dart';
