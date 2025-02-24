95568aae82c1bab0ba9a25d1098de65dc8efb3d6
ramadhanialfarizi <ramadhani.alfarizi@gmail.com>
2025-02-24 18:11:34 +0700

    add watchlist movie bloc test

---
+++ test/presentation/bloc/movie/watchlist_movie/watchlist_movie_bloc_test.dart
@@ -0,0 +1,55 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/domain/usecases/get_watchlist_movies.dart';
+import 'package:ditonton/presentation/bloc/movie/watchlist_movie/watchlist_movie_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../../../dummy_data/dummy_objects.dart';
+import 'watchlist_movie_bloc_test.mocks.dart';
+
+@GenerateMocks([
+  GetWatchlistMovies,
+])
+void main() {
+  late WatchlistMovieBloc watchlistMovieBloc;
+  late MockGetWatchlistMovies mockGetWatchlistMovies;
+
+  setUp(
+    () {
+      mockGetWatchlistMovies = MockGetWatchlistMovies();
+      watchlistMovieBloc = WatchlistMovieBloc(mockGetWatchlistMovies);
+    },
+  );
+
+  test(
+    "initial data shoul empty",
+    () {
+      expect(watchlistMovieBloc.state, WatchlistMovieEmpty());
+    },
+  );
+
+  group(
+    "loaded data from local db for watchlist",
+    () {
+      blocTest<WatchlistMovieBloc, WatchlistMovieState>(
+        "success load data",
+        build: () {
+          when(mockGetWatchlistMovies.execute())
+              .thenAnswer((_) async => Right(testMovieList));
+
+          return watchlistMovieBloc;
+        },
+        act: (bloc) => bloc.add(OnLoadWatchlistMovie()),
+        expect: () => [
+          WatchlistMovieLoading(),
+          WatchlistMovieHasData(testMovieList),
+        ],
+        verify: (bloc) {
+          verify(mockGetWatchlistMovies.execute());
+        },
+      );
+    },
+  );
+}
